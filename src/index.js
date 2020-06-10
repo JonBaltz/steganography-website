29ed142274e920a1e700afb026eb3bb2024e2e91
Jon Baltz <baltz.jon.r@gmail.com>
2020-06-10 16:30:07 -0600

    Setup server to bundle bundle react and js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <div>Bruh Status</div>
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
